4b43d99d1477a019b4e9744b34abbaebf36905a4
Ana Rodríguez <78365498+anarp14@users.noreply.github.com>
2023-05-15 19:35:10 +0200

    Add files via upload
    Funciona pero hay que refactorizarlo porque se repite código

--- public/index.php
+++ public/index.php
@@ -186,6 +186,17 @@
         </div>
         <div class="flex">
             <main class="flex-1 grid grid-cols-3 gap-4 justify-center justify-items-center">
+                <?php
+                $usuario = \App\Tablas\Usuario::logueado();
+                $usuario_id = $usuario ? $usuario->id : null;
+
+                $facturas = Factura::todosConTotal(
+                    ['usuario_id = :usuario_id'],
+                    [':usuario_id' => $usuario_id]
+                );
+
+
+                ?>
                 <?php foreach ($sent as $fila) : ?>
                     <div class="p-6 max-w-xs min-w-full bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
                         <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"><?= hh($fila['descripcion']) ?> - <?= hh($fila['precio']) ?> € </h5>
@@ -204,65 +215,54 @@
                             </a>
                         <?php endif ?>
                         <div class="flex mb-3 font-normal text-gray-700 dark:text-gray-400">
-                            <?php
-                            $usuario = \App\Tablas\Usuario::logueado();
-                            $usuario_id = $usuario ? $usuario->id : null;
+                            <?php if (!empty($facturas)) : ?>
 
-                            if ($usuario) {
-                                $facturas = Factura::todosConTotal(
-                                    ['usuario_id = :usuario_id'],
-                                    [':usuario_id' => $usuario_id]
-                                );
-                            } else {
-                                $facturas = []; // Si el usuario no está logeado, inicializa el array de facturas vacío
-                            }
-                            ?>
-                            <?php foreach ($facturas as $factura) : ?>
-                                <?php
-                                $articuloId = $fila['id']; // Obtén el ID del artículo actual
+                                <?php foreach ($facturas as $factura) : ?>
+                                    <?php
+                                    $articuloId = $fila['id']; // Obtén el ID del artículo actual
 
-                                // Verifica si el artículo está comprado utilizando el método seHaComprado
-                                $estaComprado = $factura->seHaComprado($articuloId);
-                                ?>
-                                <form action="valorar_articulo.php" method="GET">
-                                    <label class="block mb-2 text-sm font-medium w-1/4 pr-4">
-                                        Valoración:
-                                        <?php
-                                        $sent3 = $pdo->prepare("SELECT *
+                                    // Verifica si el artículo está comprado utilizando el método seHaComprado
+                                    $estaComprado = $factura->seHaComprado($articuloId);
+                                    ?>
+                                    <form action="valorar_articulo.php" method="GET">
+                                        <label class="block mb-2 text-sm font-medium w-1/4 pr-4">
+                                            Valoración:
+                                            <?php
+                                            $sent3 = $pdo->prepare("SELECT *
                                                         FROM valoraciones
                                                         WHERE usuario_id = :usuario_id AND articulo_id = :articulo_id");
-                                        $sent3->execute(['usuario_id' => $usuario_id, 'articulo_id' => $fila['id']]);
-                                        $valoracion_usuario = $sent3->fetch(PDO::FETCH_ASSOC);
-                                        ?>
-                                        <select name="valoracion" id="valoracion">
-                                            <option value="" <?= (!$usuario_id) ? 'selected' : '' ?>></option>
-                                            <?php for ($i = 1; $i <= 5; $i++) : ?>
-                                                <option value="<?= $i ?>" <?= ($valoracion_usuario && $valoracion_usuario['valoracion'] == $i) ? 'selected' : '' ?>><?= $i ?></option>
-                                            <?php endfor ?>
-                                        </select>
-                                    </label>
-                                    <input type="hidden" name="articulo_id" value="<?= $fila['id'] ?>">
-                                    <input type="hidden" name="usuario_id" value="<?= $usuario_id ?>">
+                                            $sent3->execute(['usuario_id' => $usuario_id, 'articulo_id' => $fila['id']]);
+                                            $valoracion_usuario = $sent3->fetch(PDO::FETCH_ASSOC);
+                                            ?>
+                                            <select name="valoracion" id="valoracion">
+                                                <option value="" <?= (!$usuario_id) ? 'selected' : '' ?>></option>
+                                                <?php for ($i = 1; $i <= 5; $i++) : ?>
+                                                    <option value="<?= $i ?>" <?= ($valoracion_usuario && $valoracion_usuario['valoracion'] == $i) ? 'selected' : '' ?>><?= $i ?></option>
+                                                <?php endfor ?>
+                                            </select>
+                                        </label>
+                                        <input type="hidden" name="articulo_id" value="<?= $fila['id'] ?>">
+                                        <input type="hidden" name="usuario_id" value="<?= $usuario_id ?>">
 
-                                    <?php if (!(\App\Tablas\Usuario::esta_logueado() && $estaComprado)) : ?>
-                                        <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" disabled>Votar</button>
-                                    <?php else : ?>
-                                        <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Votar</button>
-                                    <?php endif ?>
-                                </form>
-                                <div>
-                                    <label class="block text-m font-medium pl-3 ml-3">
-                                        Valoración media:
-                                        <?php
-                                        $sent4 = $pdo->prepare("SELECT avg(valoracion)::numeric(10,2)
+                                        <?php if (!(\App\Tablas\Usuario::esta_logueado() && $estaComprado)) : ?>
+                                            <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" disabled>Votar</button>
+                                        <?php else : ?>
+                                            <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Votar</button>
+                                        <?php endif ?>
+                                    </form>
+                                    <div>
+                                        <label class="block text-m font-medium pl-3 ml-3">
+                                            Valoración media:
+                                            <?php
+                                            $sent4 = $pdo->prepare("SELECT avg(valoracion)::numeric(10,2)
                                                         FROM valoraciones
                                                         WHERE articulo_id = :articulo_id");
-                                        $sent4->execute(['articulo_id' => $fila['id']]);
-                                        $valoracionMedia = $sent4->fetchColumn();
-                                        ?>
-                                        <p class="mb-3 pl-3 font-normal text-gray-700 dark:text-gray-400"><?= hh($valoracionMedia) ?></p>
-                                    </label>
-                                </div>
+                                            $sent4->execute(['articulo_id' => $fila['id']]);
+                                            $valoracionMedia = $sent4->fetchColumn();
+                                            ?>
+                                            <p class="mb-3 pl-3 font-normal text-gray-700 dark:text-gray-400"><?= hh($valoracionMedia) ?></p>
+                                        </label>
+                                    </div>
                         </div>
 
                         <form action="comentar_articulo.php" method="POST" class="inline">
@@ -276,43 +276,92 @@
                             <?php endif ?>
                         </form>
                     <?php endforeach ?>
-                    </div>
-                <?php endforeach ?>
-            </main>
+                <?php else : ?>
+                    <form action="valorar_articulo.php" method="GET">
+                        <label class="block mb-2 text-sm font-medium w-1/4 pr-4">
+                            Valoración:
+                            <?php
+                                $usuario = \App\Tablas\Usuario::logueado();
+                                $usuario_id = $usuario ? $usuario->id : null;
 
-            <?php if (!$carrito->vacio()) : ?>
-                <aside class="flex flex-col items-center w-1/4" aria-label="Sidebar">
-                    <div class="overflow-y-auto py-4 px-3 bg-gray-50 rounded dark:bg-gray-800">
-                        <table class="mx-auto text-sm text-left text-gray-500 dark:text-gray-400">
-                            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                                <th scope="col" class="py-3 px-6">Descripción</th>
-                                <th scope="col" class="py-3 px-6">Cantidad</th>
-                            </thead>
-                            <tbody>
-                                <?php foreach ($carrito->getLineas() as $id => $linea) : ?>
-                                    <?php
-                                    $articulo = $linea->getArticulo();
-                                    $cantidad = $linea->getCantidad();
-                                    ?>
-                                    <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                                        <td class="py-4 px-6"><?= $articulo->getDescripcion() ?> <br>
-                                            <?= $articulo->getCategoriaNombre($pdo) ?>
-                                            <?= $articulo->getEtiquetaNombre($pdo) ?>
+                                $sent3 = $pdo->prepare("SELECT *
+                                                        FROM valoraciones
+                                                        WHERE usuario_id = :usuario_id AND articulo_id = :articulo_id");
+                                $sent3->execute(['usuario_id' => $usuario_id, 'articulo_id' => $fila['id']]);
+                                $valoracion_usuario = $sent3->fetch(PDO::FETCH_ASSOC);
+                            ?>
+                            <select name="valoracion" id="valoracion">
+                                <option value="" <?= (!$usuario_id) ? 'selected' : '' ?>></option>
+                                <?php for ($i = 1; $i <= 5; $i++) : ?>
+                                    <option value="<?= $i ?>" <?= ($valoracion_usuario && $valoracion_usuario['valoracion'] == $i) ? 'selected' : '' ?>><?= $i ?></option>
+                                <?php endfor ?>
+                            </select>
+                        </label>
+                        <input type="hidden" name="articulo_id" value="<?= $fila['id'] ?>">
+                        <input type="hidden" name="usuario_id" value="<?= $usuario_id ?>">
 
-                                        </td>
-                                        <td class="py-4 px-6 text-center"><?= $cantidad ?></td>
-                                    </tr>
-                                <?php endforeach ?>
-                            </tbody>
-                        </table>
-                    </div>
+
+                        <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" disabled>Votar</button>
+
+                    </form>
                     <div>
-                        <a href="/vaciar_carrito.php" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Vaciar carrito</a>
-                        <a href="/comprar.php" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-900">Comprar</a>
+                        <label class="block text-m font-medium pl-3 ml-3">
+                            Valoración media:
+                            <?php
+                                $sent4 = $pdo->prepare("SELECT avg(valoracion)::numeric(10,2)
+                                                        FROM valoraciones
+                                                        WHERE articulo_id = :articulo_id");
+                                $sent4->execute(['articulo_id' => $fila['id']]);
+                                $valoracionMedia = $sent4->fetchColumn();
+                            ?>
+                            <p class="mb-3 pl-3 font-normal text-gray-700 dark:text-gray-400"><?= hh($valoracionMedia) ?></p>
+                        </label>
                     </div>
-                </aside>
-            <?php endif ?>
+                    </div>
+
+                    <form action="comentar_articulo.php" method="POST" class="inline">
+                        <input type="hidden" name="articulo_id" value="<?= $fila['id'] ?>">
+                        <input type="hidden" name="usuario_id" value="<?= $usuario_id ?>">
+                        <button type="submit" disabled onclick="cambiar(event, <?= $fila['id'] ?>, <?= $usuario_id ?>)" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-4 py-2 mr-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-900" data-modal-toggle="insertar_comentario">Comentar</button>
+                    </form>
+                <?php endif ?>
         </div>
+    <?php endforeach ?>
+    </main>
+
+    <?php if (!$carrito->vacio()) : ?>
+        <aside class="flex flex-col items-center w-1/4" aria-label="Sidebar">
+            <div class="overflow-y-auto py-4 px-3 bg-gray-50 rounded dark:bg-gray-800">
+                <table class="mx-auto text-sm text-left text-gray-500 dark:text-gray-400">
+                    <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                        <th scope="col" class="py-3 px-6">Descripción</th>
+                        <th scope="col" class="py-3 px-6">Cantidad</th>
+                    </thead>
+                    <tbody>
+                        <?php foreach ($carrito->getLineas() as $id => $linea) : ?>
+                            <?php
+                            $articulo = $linea->getArticulo();
+                            $cantidad = $linea->getCantidad();
+                            ?>
+                            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                                <td class="py-4 px-6"><?= $articulo->getDescripcion() ?> <br>
+                                    <?= $articulo->getCategoriaNombre($pdo) ?>
+                                    <?= $articulo->getEtiquetaNombre($pdo) ?>
+
+                                </td>
+                                <td class="py-4 px-6 text-center"><?= $cantidad ?></td>
+                            </tr>
+                        <?php endforeach ?>
+                    </tbody>
+                </table>
+            </div>
+            <div>
+                <a href="/vaciar_carrito.php" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Vaciar carrito</a>
+                <a href="/comprar.php" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-900">Comprar</a>
+            </div>
+        </aside>
+    <?php endif ?>
+    </div>
     </div>
     <!-- Esto es para añadir un nuevo comentario -->
     <div id="insertar_comentario" tabindex="-1" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 md:inset-0 h-modal md:h-full">
